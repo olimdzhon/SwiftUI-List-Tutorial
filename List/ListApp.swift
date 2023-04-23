99ad2e309a8202fb11c8649f175a01543f98fcd4
Olimdzhon <write2sd@mail.ru>
2023-04-23 18:41:04 +0500

    Initial Commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Олимджон Садыков on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
